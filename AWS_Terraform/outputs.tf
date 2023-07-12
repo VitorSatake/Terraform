8ff8b7fe6cc24278b5a4a72e17a370512b645574
Vitor Satake <117522736+VitorSatake@users.noreply.github.com>
2023-07-12 16:17:07 -0300

    Create outputs

---
+++ AWS_Terraform/outputs.tf
@@ -0,0 +1,13 @@
+// define dados de saída da instância
+// no caso ID da instância e IP público
+
+output "instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.app_server.id
+}
+
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.app_server.public_ip
+}
+
